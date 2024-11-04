0755297b44bdd65909d69a4534e2b016a6ec8ba7
shipclone06 <anhkhoa030900@gmail.com>
2024-11-04 11:23:46 +0700

    Add project files.

---
+++ bai1_helloworld1/bai1_helloworld1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "hello world!\n";
+    cout << "toi ten la_" << "nguyen anh khoa" << endl;
+    cout << "toi dang hoc lap trinh\tc++" << endl;
+    
+    return 0;
+}
